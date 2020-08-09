1a69e44d22114452f21464a268095a7ff9f00cf4
Marcus Franco <rasfmar@rasfmar-2.local>
2020-08-09 15:33:41 -0400

    woo

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,13 @@ import React from "react";
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script src="https://cdn.jsdelivr.net/npm/kursor"/>,
-    <script dangerouslySetInnerHTML={{ __html: `new kursor({ type: 1, color: "#fff" });` }}/>
+    <script dangerouslySetInnerHTML={{ __html: `new kursor({ type: 1, color: "#fff" });` }}/>,
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156555564-2"/>,
+    <script dangerouslySetInnerHTML={{ __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-156555564-2');`
+    }}/>
   ])
 }
